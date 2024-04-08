024cd90360fabdc15f75e9949af5fd1f9b16f024
Al-amin07 <alsujon2001@gmail.com>
2024-04-08 23:36:06 +0600

    Register is added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,15 @@
 import { useLoaderData } from "react-router-dom";
 import Card from "./Card";
+import Banner from "./Banner";
 
 const Home = () => {
   const allData = useLoaderData();
   console.log(allData);
   return (
     <div>
-      <div>banner</div>
+      <div>
+        <Banner></Banner>
+        </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
         {allData.map((datas) => {
           return datas.map((data, i) => <Card key={i} data={data}></Card>);
